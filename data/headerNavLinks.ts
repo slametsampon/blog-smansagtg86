e32bc9cb57dee14af981c8919cb47d06808a0228
Slamet <slamet@pon.co.id>
2023-11-22 11:11:08 +0700

    update menu & about

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/bisnis', title: 'Bisnis' },
   { href: '/blog', title: 'Blog' },
-  { href: '/reuni', title: 'Reuni' },
+  { href: '/cerita-foto', title: 'Cerita Dibalik Foto' },
   { href: '/about', title: 'About' },
 ]
 
